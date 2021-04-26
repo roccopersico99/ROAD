4a187863dd716c01c46db4e671bcdbb001a8466a
Ryan Chung <ryan.chung@stonybrook.edu>
2021-04-25 20:24:47 -0400

    added a benchmark3 folder

---
+++ public/benchmark3/src/Homework4/Player/PlayerStates/Idle.ts
@@ -0,0 +1,38 @@
+import Input from "../../../Wolfie2D/Input/Input";
+import AnimatedSprite from "../../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import { PlayerStates } from "../PlayerController";
+import { HW4_Events } from "c:/Users/ryanj/Documents/COLLEGENOTES/CSE/CSE380/HW4/src/Homework4/hw4_enums";
+import OnGround from "./OnGround";
+
+export default class Idle extends OnGround {
+	owner: AnimatedSprite;
+
+	onEnter(options: Record<string, any>): void {
+		this.parent.speed = this.parent.MIN_SPEED;
+		this.owner.animation.play("IDLE", true);
+	}
+
+	update(deltaT: number): void {
+		super.update(deltaT);
+
+		let dir = this.getInputDirection();
+
+		if(!dir.isZero() && dir.y === 0){
+			if(Input.isPressed("run")){
+				this.finished(PlayerStates.RUN);
+			} else {
+				this.finished(PlayerStates.WALK);
+			}
+		}
+		
+		this.parent.velocity.x = 0;
+
+		this.emitter.fireEvent(HW4_Events.PLAYER_MOVE, {position: this.owner.position.clone()});
+		this.owner.move(this.parent.velocity.scaled(deltaT));
+	}
+
+	onExit(): Record<string, any> {
+		this.owner.animation.stop();
+		return {};
+	}
+}
